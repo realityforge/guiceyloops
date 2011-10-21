efbdb2f48faeb169a1d785d5c338c7968106133c
Peter Donald <peter@realityforge.org>
2011-10-21 22:15:32 +1100

    Add some minimal javadoc

--- src/main/java/org/realityforge/guiceyloops/JEETestingModule.java
+++ src/main/java/org/realityforge/guiceyloops/JEETestingModule.java
@@ -3,9 +3,15 @@ package org.realityforge.guiceyloops;
 import com.google.inject.AbstractModule;
 import com.google.inject.matcher.Matchers;
 
+/**
+ * The Guice module that should be added to the injector to add annotation listeners.
+ */
 public class JEETestingModule
     extends AbstractModule
 {
+  /**
+   * {@inheritDoc}
+   */
   protected void configure()
   {
     bindListener( Matchers.any(), new JpaTypeListener() );
