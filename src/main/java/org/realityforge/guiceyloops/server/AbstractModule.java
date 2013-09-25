193618d90fbf7f058f6db23ceaf38cd804ee778d
Peter Donald <peter@realityforge.org>
2013-09-25 15:48:14 +1000

    Extract out superclass for ee modules

---
+++ src/main/java/org/realityforge/guiceyloops/server/AbstractModule.java
@@ -0,0 +1,63 @@
+package org.realityforge.guiceyloops.server;
+
+import com.google.inject.name.Names;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import javax.inject.Singleton;
+import org.mockito.Mockito;
+
+public abstract class AbstractModule
+  extends com.google.inject.AbstractModule
+{
+  @SuppressWarnings( "unchecked" )
+  protected final void multiBind( final Class implementation, final Class... interfaces )
+  {
+    final Object impl = instantiate( implementation );
+    requestInjection( impl );
+    bind( interfaces[ 0 ] ).toInstance( impl );
+
+    for ( int i = 1; i < interfaces.length; i++ )
+    {
+      final ClassLoader classLoader = getClass().getClassLoader();
+      final Class type = interfaces[ i ];
+      final InvocationHandler handler = new InvocationHandler()
+      {
+        @Override
+        public Object invoke( final Object proxy, final Method method, final Object[] args )
+          throws Throwable
+        {
+          return method.invoke( impl, args );
+        }
+      };
+      bind( type ).toInstance( Proxy.newProxyInstance( classLoader, new Class[]{ type }, handler ) );
+    }
+  }
+
+  private Object instantiate( final Class implementation )
+  {
+    try
+    {
+      return implementation.newInstance();
+    }
+    catch ( final Throwable throwable )
+    {
+      throw new IllegalStateException( throwable.getMessage(), throwable );
+    }
+  }
+
+  protected final <T> void bindMock( final Class<T> type )
+  {
+    bind( type ).toInstance( Mockito.mock( type ) );
+  }
+
+  protected final <T> void bindResource( final Class<T> resultType, final String name, final T instance )
+  {
+    bind( resultType ).annotatedWith( Names.named( name ) ).toInstance( instance );
+  }
+
+  protected final <T> void bindSingleton( final Class<T> service, final Class<? extends T> implementation )
+  {
+    bind( service ).to( implementation ).in( Singleton.class );
+  }
+}
