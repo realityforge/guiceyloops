0bf54836874decafe75ed82a86174e9ddd8b526f
Peter Donald <peter@realityforge.org>
2013-11-01 13:37:45 +1100

    Extract out a superclass that encapsulates the flushing behaviour

---
+++ src/main/java/org/realityforge/guiceyloops/server/FlushingTestModule.java
@@ -0,0 +1,32 @@
+package org.realityforge.guiceyloops.server;
+
+import com.google.inject.matcher.AbstractMatcher;
+import static com.google.inject.matcher.Matchers.any;
+
+/**
+ * The module that provides a will flush before and after invoking the service methods.
+ */
+public abstract class FlushingTestModule
+  extends AbstractModule
+{
+  private final FlushingInterceptor _interceptor;
+
+  public FlushingTestModule( final Flushable flushable )
+  {
+    _interceptor = new FlushingInterceptor( flushable );
+  }
+
+  protected final <T> void bindService( final Class<T> service,
+                                        final Class<? extends T> implementation )
+  {
+    bindSingleton( service, implementation );
+    bindInterceptor( new AbstractMatcher<Class<?>>()
+    {
+      @Override
+      public boolean matches( final Class<?> aClass )
+      {
+        return implementation == aClass;
+      }
+    }, any(), _interceptor );
+  }
+}
--- src/main/java/org/realityforge/guiceyloops/server/ServerTestModule.java
+++ src/main/java/org/realityforge/guiceyloops/server/ServerTestModule.java
@@ -1,21 +1,17 @@
 package org.realityforge.guiceyloops.server;
 
-import com.google.inject.matcher.AbstractMatcher;
 import javax.ejb.SessionContext;
 import javax.transaction.TransactionSynchronizationRegistry;
-import static com.google.inject.matcher.Matchers.any;
 
 /**
  * The client module for all the server side tests in absence of a specific module for a test.
  */
 public class ServerTestModule
-  extends AbstractModule
+  extends FlushingTestModule
 {
-  private final FlushingInterceptor _interceptor;
-
   public ServerTestModule( final Flushable flushable )
   {
-    _interceptor = new FlushingInterceptor( flushable );
+    super( flushable );
   }
 
   @Override
@@ -23,19 +19,5 @@ public class ServerTestModule
   {
     bindMock( SessionContext.class );
     bindService( TransactionSynchronizationRegistry.class, TestTransactionSynchronizationRegistry.class );
-  }
-
-  protected <T> void bindService( final Class<T> service,
-                                  final Class<? extends T> implementation )
-  {
-    bindSingleton( service, implementation );
-    bindInterceptor( new AbstractMatcher<Class<?>>()
-    {
-      @Override
-      public boolean matches( final Class<?> aClass )
-      {
-        return implementation == aClass;
-      }
-    }, any(), _interceptor );
   }
 }
