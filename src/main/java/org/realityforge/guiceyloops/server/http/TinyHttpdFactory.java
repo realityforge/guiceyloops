492b3a5c844997c8c0ac944503c92cd177797e9a
Peter Donald <peter@realityforge.org>
2016-06-22 16:04:42 +1000

    Support named servers in TinyHttpdFactory

--- src/main/java/org/realityforge/guiceyloops/server/http/TinyHttpdFactory.java
+++ src/main/java/org/realityforge/guiceyloops/server/http/TinyHttpdFactory.java
@@ -1,16 +1,37 @@
 package org.realityforge.guiceyloops.server.http;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Nullable;
 
 public final class TinyHttpdFactory
 {
   private static final ArrayList<TinyHttpd> c_httpServers = new ArrayList<>();
+  private static final Map<String, TinyHttpd> c_httpServerMap = new HashMap<>();
 
   public static TinyHttpd createServer()
   {
+    return findOrCreateServer( null );
+  }
+
+  public static TinyHttpd findOrCreateServer( @Nullable final String key )
+  {
+    if ( null != key )
+    {
+      final TinyHttpd httpd = c_httpServerMap.get( key );
+      if ( null != httpd )
+      {
+        return httpd;
+      }
+    }
     try
     {
       final TinyHttpd httpd = new TinyHttpd();
+      if ( null != key )
+      {
+        c_httpServerMap.put( key, httpd );
+      }
       c_httpServers.add( httpd );
       return httpd;
     }
@@ -41,5 +62,6 @@ public final class TinyHttpdFactory
   {
     shutdownServers();
     c_httpServers.clear();
+    c_httpServerMap.clear();
   }
 }
