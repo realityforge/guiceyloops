fe43ca4478c206d6b5a1a424e58e96ee5fad84d3
Peter Donald <peter@realityforge.org>
2020-08-27 15:48:19 +1000

    Add nullability annotations

--- src/main/java/org/realityforge/guiceyloops/server/FlushingTestModule.java
+++ src/main/java/org/realityforge/guiceyloops/server/FlushingTestModule.java
@@ -1,6 +1,7 @@
 package org.realityforge.guiceyloops.server;
 
 import com.google.inject.matcher.AbstractMatcher;
+import javax.annotation.Nonnull;
 import org.realityforge.guiceyloops.shared.AbstractModule;
 import static com.google.inject.matcher.Matchers.any;
 
@@ -39,8 +40,8 @@ public abstract class FlushingTestModule
     }
   }
 
-  protected final <T> void bindService( final Class<T> service,
-                                        final Class<? extends T> implementation )
+  protected final <T> void bindService( @Nonnull final Class<T> service,
+                                        @Nonnull final Class<? extends T> implementation )
   {
     bindSingleton( service, implementation );
     bindInterceptor( new AbstractMatcher<Class<?>>()
